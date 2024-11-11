2e8c84b71a925d627ec7117875ee5e4a1fedc021
Kuga09 <glal64@tutamail.com>
2024-11-11 15:28:33 +0300

    Задание 2

--- search.py
+++ search.py
@@ -1,9 +1,11 @@
 from random import randint as ri
+ #Создание списка случайными числами
 start_list = []
-for i in range(0,100):
+for i in range(0,100): 
     start_list.append(ri(0, 1_000_000))
 unsorted_list = start_list
 sorted_list = []
+#Сортировка этого списка
 while len(unsorted_list) > 0:
     min = unsorted_list[0]
     for i in range(0, len(unsorted_list)):
@@ -11,28 +13,30 @@ while len(unsorted_list) > 0:
             min = unsorted_list[i]
     sorted_list.append(min)
     unsorted_list.remove(min)
+#Вывод исходного списка
 print('Исходный список:')
 print(sorted_list)
+#Ввод искомого числа
 print('Введите число, которое следует найти')
 x=int(input())
-k1=0
-
-for i in range(len(sorted_list)):
+#Линейный поиск
+count_1=0
+for i in range(len(sorted_list)): 
     if sorted_list[i] == x:
         break
-    k1+=1
+    count_1+=1
 if sorted_list[i] != x:
     print('Элемент не найден')
 else:
     print('Индекс элемента, найденный алгоритмом "Линейный поиск" =',i)
-print('Количество сравнений =',k1)
+print('Количество сравнений =',count_1)
 print(' ')
-
-k2=0
+#Бинарный поиск
+count_2=0
 first = 0
 last = len(sorted_list)-1
 index = -1
-while (first <= last) and (index == -1):
+while (first <= last) and (index == -1): 
     mid = (first+last)//2
     if sorted_list[mid] == x:
         index = mid
@@ -41,9 +45,9 @@ while (first <= last) and (index == -1):
             last = mid -1
         else:
             first = mid +1
-    k2+=1
+    count_2+=1
 if index == -1:
     print('Элемент не найден')
 else:
     print('Индекс элемента, найденный алгоритмом "Бинарный поиск" =',index)
-print('Количество сравнений =',k2)
+print('Количество сравнений =',count_2)
